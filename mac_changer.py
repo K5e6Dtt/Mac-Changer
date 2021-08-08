4736f701ce986ce586a87a788e88e4e1aa365ae1
Kuntal Dutta <88590817+K5e6Dtt@users.noreply.github.com>
2021-08-08 12:48:00 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+#Needs root privilege
+#Only works in Linux
 
-import subprocess,argparse,sys,re
+import subprocess,argparse,sys,re,random
 from termcolor import colored
 
 def get_arguments():
-    parser = argparse.ArgumentParser(description="A tool for changing mac addresses")
-    parser.add_argument('--interface',dest="interface",help="The network interface")
-    parser.add_argument('--mac',dest="mac",help="The new MAC addresses")
+    parser = argparse.ArgumentParser(description="A tool for changing mac addresses, If mac not given the script will generate a random mac")
+    parser.add_argument('-i','--interface',dest="interface",help="The network interface")
+    parser.add_argument('-m','--mac',dest="mac",help="The new MAC addresses")
     options = parser.parse_args()
-    if not options.interface or not options.mac:
-        print(colored(f"One or more of the options were not given.\nType {sys.argv[0]} --help for usage.",'red'))
+    if not options.interface:
+        print(colored(f"Give interface to change it's mac.\nType {sys.argv[0]} --help or -h for usage.",'red'))
         sys.exit(0)
     else:
         return options
@@ -19,10 +21,23 @@ def mac_len_checker(mac):
         print(colored("[-] Insufficient number of characters in the mac address, try with a full mac address.",'red'))
         sys.exit(0)   
 
+
+def rand_mac():
+    mac =  "%02x:%02x:%02x:%02x:%02x:%02x" % (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255)
+        )
+    return mac
+
+
 def mac_checker(interface):
     mac_address_result = subprocess.check_output(['ifconfig',interface])
     mac = mac_address_result.decode()
-    mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",mac) #checking correct mac order using regex
+    mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",mac)
     mac = mac.group(0)
     return mac
 
@@ -36,7 +51,10 @@ def mac_changer(interface,mac):
 def main():
     options = get_arguments()
     interface = options.interface
-    mac = options.mac
+    if options.mac:
+        mac = options.mac
+    else:
+        mac = rand_mac()    
     mac_len_checker(mac)
     try:    
         before_change = mac_checker(interface)
@@ -45,13 +63,16 @@ def main():
         if before_change != after_change:
             print(colored("[+] MAC address changed successfully.",'green'))
         else:
-            print(colored("[+] MAC is same,something went wrong please try again",'red'))
+            print(colored("[-] MAC is same,something went wrong please try again",'red'))
     except:
         print(colored("Error! Incorrect interface!!!",'red'))
         
 
 if __name__ == '__main__':
     main()
+
+
+
 
 
 
